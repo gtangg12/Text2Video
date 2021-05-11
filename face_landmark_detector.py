8875e70dbb852a97320a566ddbd4523ae2a0e342
George Tang <gtangg12gmail.com>
2021-05-11 15:17:54 -0400

    dump

--- face_landmark_detector.py
+++ face_landmark_detector.py
@@ -7,7 +7,7 @@ data_dir = 'face_landmark_data/'
 face_detector = dlib.get_frontal_face_detector()
 landmark_detector = dlib.shape_predictor(f'{data_dir}/shape_predictor_68_face_landmarks.dat')
 
-img = cv.imread(f'{data_dir}/down.png')
+img = cv.imread(f'{data_dir}/me11.png')
 print(img.shape)
 img = cv.resize(img, (500, 750))
 
@@ -17,11 +17,12 @@ landmark_coords = []
 
 for k, d in enumerate(faces):
     landmarks = landmark_detector(img, d)
-    for n in range(48, 68):
+    for n in [36, 45, 30, 57]: #range(18, 68):
         x = landmarks.part(n).x
         y = landmarks.part(n).y
         landmark_coords.append((x, y))
         # draw circle on image at landmark
-        cv.circle(img, (x, y), 2, (255, 255, 0), -1)
+        cv.circle(img, (x, y), 3, (255, 255, 0), -1)
+    print(landmark_coords)
 
-cv.imwrite('down_out.png', img)
+cv.imwrite('me11_out.png', img)
