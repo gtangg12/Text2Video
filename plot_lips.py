245ee9b17fe184917439c4001f16164dca7df234
William Zhao <williamzhaohaitong7@gmail.com>
2021-05-21 00:25:15 -0400

    more fine tuning experiments

--- plot_lips.py
+++ plot_lips.py
@@ -1,8 +1,8 @@
 import numpy as np
 import cv2 
 from tqdm import tqdm
-sample_lips = np.load('downsample_preds_reprtemp.npy')
-sample_times = np.load('downsample_times_reprtemp.npy')
+sample_lips = np.load('downsample_preds_rts.npy')
+sample_times = np.load('downsample_times_rts.npy')
 sample_lips = sample_lips.reshape(-1, 18, 2)
 #frames = range(13568, 15261)
 for t, lip in tqdm(zip(sample_times, sample_lips)):
@@ -13,4 +13,4 @@ for t, lip in tqdm(zip(sample_times, sample_lips)):
     #cv2.rectangle(im, (80, 130), (144, 180), (0,0,0), cv2.FILLED)
     for point in lip_points:
         cv2.circle(im, (round(point[0]), round(point[1])), 2, (255, 0, 0), cv2.FILLED)
-    cv2.imwrite(f'/media/william/DATA/6869/output_rnn/reprtemp/frame_{f}.jpg', im)
+    cv2.imwrite(f'/media/william/DATA/6869/output_rnn/rts2/frame_{f}.jpg', im)
